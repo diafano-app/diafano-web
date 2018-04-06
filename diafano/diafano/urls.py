43dcb93f680c5fd8c6b3d4020f0fde70ca9a5d38
mertaytore <aytoremert@gmail.com>
2018-04-06 23:15:31 +0300

    settings: urls and settings changed for landing

--- diafano/diafano/urls.py
+++ diafano/diafano/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
+# from . import views
 
 urlpatterns = [
-    path('', views.signIn),
-    path('signup/', views.signUp, name = 'signup'),
-    path('postsignup/', views.postsignup, name = 'postsignup'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('homepage.urls'))
 ]
