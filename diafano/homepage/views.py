ef46ca0678ae7cc527225459be84d0b69894e631
mertaytore <aytoremert@gmail.com>
2018-04-25 19:30:41 +0300

    views: logout and login progress

--- diafano/homepage/views.py
+++ diafano/homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+#from django.contrib.auth.views import logout
 from django.contrib.auth.models import User
 
 import pyrebase
@@ -27,11 +28,24 @@ database = firebase.database()
 def landing(request):
     return render(request,"landing.html", {"mapbox_access_token" : config['mapboxgl']['accessToken']})
 
+def logout(request):
+    [sp.session.delete() for sp in SessionProfile.objects]
+    logout(request)
+    return landing(request)
+
 def signIn(request):
     return render(request,"signIn.html", {"mapbox_access_token" : config['mapboxgl']['accessToken']})
 
 def signUp(request):
-    return render(request,"signup.html", {"mapbox_access_token" : config['mapboxgl']['accessToken']})
+    if not request.user.is_authenticated:
+        return render(request,"signup.html", {"mapbox_access_token" : config['mapboxgl']['accessToken']})
+    else:
+        return render(request,"dashboard.html", {
+            "firebase_apikey" : config['firebaseConfig']['apiKey'],
+            "mapbox_access_token" : config['mapboxgl']['accessToken'],
+            "firebase_authdomain" : config['firebaseConfig']['authDomain'],
+            "firebase_dburl" : config['firebaseConfig']['databaseURL'],
+            "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
 
 def dashboard(request):
     return render(request,"dashboard.html", {
@@ -41,14 +55,8 @@ def dashboard(request):
         "firebase_dburl" : config['firebaseConfig']['databaseURL'],
         "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
 
-def home(request):
-    return render(request, 'home.html')
-
-def login(request):
-    return render(request, 'log.html')
-
 def postsignup(request):
-    name = request.POST.get('email')
+    name = request.POST.get('name')
     email = request.POST.get('email')
     passw = request.POST.get('pass')
     try:
@@ -58,9 +66,7 @@ def postsignup(request):
         message = "Unable to create account try again"
         return render(request, "landing.html", { "messg" : message, "mapbox_access_token" : config['mapboxgl']['accessToken'] })
         uid = user['localId']
-
-    local_user = User.objects.create_user(name, email, passw)
-
+    local_user = User.objects.create_user(email, email, passw)
     location = {
         "latitude" : -1,
         "long" : -1
@@ -125,60 +131,65 @@ def postsignup_google(request):
         "firebase_authdomain" : config['firebaseConfig']['authDomain'],
         "firebase_dburl" : config['firebaseConfig']['databaseURL'],
         "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
-    
+
 def postsignin(request):
-    email = request.POST.get('username')
-    passw = request.POST.get('pass')
-    print("email: {} password: {}".format(email, passw))
-    try:
-        user = auth.sign_in_with_email_and_password(email, passw)
-        #uid = user['localId']
-        user = authenticate(request, username=email, password=passw)
-        if user is not None:
-            login( user)
-        else:
-            local_user = User.objects.create_user(email, email, passw)
-        # Return an 'invalid login' error message.
-            
-        return render(request,"dashboard.html", {
+    if not request.session._session:
+        email = request.POST.get('username')
+        passw = request.POST.get('pass')
+        try:
+            user = auth.sign_in_with_email_and_password(email, passw)
+            if user is None:
+                message = "Unable to sign in to account try again"
+                return render(request, "landing.html", { "messg" : message, "mapbox_access_token" : config['mapboxgl']['accessToken'] })
+
+            #uid = user['localId']
+            local_user = authenticate(request, username=email, password=passw)
+            if local_user is not None:
+                if not local_user.is_active:
+                    login(local_user)
+            else:
+                local_user = User.objects.create_user(email, email, passw)
+            # Return an 'invalid login' error message.
+
+            return render(request,"dashboard.html", {
+                "firebase_apikey" : config['firebaseConfig']['apiKey'],
+                "mapbox_access_token" : config['mapboxgl']['accessToken'],
+                "firebase_authdomain" : config['firebaseConfig']['authDomain'],
+                "firebase_dburl" : config['firebaseConfig']['databaseURL'],
+                "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
+        except:
+            message = "Unable to sign in to account try again"
+            return render(request, "landing.html", { "messg" : message, "mapbox_access_token" : config['mapboxgl']['accessToken'] })
+    return render(request,"dashboard.html", {
         "firebase_apikey" : config['firebaseConfig']['apiKey'],
         "mapbox_access_token" : config['mapboxgl']['accessToken'],
         "firebase_authdomain" : config['firebaseConfig']['authDomain'],
         "firebase_dburl" : config['firebaseConfig']['databaseURL'],
         "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
-    except:
-        message = "Unable to sign in to account try again"
-        return render(request, "landing.html", { "messg" : message, "mapbox_access_token" : config['mapboxgl']['accessToken'] })
 
 def postsignin_google(request):
-    current_user = request.user
-    name = current_user.username
-    email = current_user.email
-    passw = current_user.email
+    if not request.user.is_authenticated:
+        current_user = request.user
+        name = current_user.username
+        email = current_user.email
+        passw = current_user.email
 
-    try:
-        user = auth.sign_in_with_email_and_password(email, passw)
-        #uid = user['localId']
-        user = authenticate(request, username=email, password=passw)
-        if user is not None:
-            login( user)
-        else:
-            local_user = User.objects.create_user(email, email, passw)
-        # Return an 'invalid login' error message.
-            
-        return render(request,"dashboard.html", {
-        "firebase_apikey" : config['firebaseConfig']['apiKey'],
-        "mapbox_access_token" : config['mapboxgl']['accessToken'],
-        "firebase_authdomain" : config['firebaseConfig']['authDomain'],
-        "firebase_dburl" : config['firebaseConfig']['databaseURL'],
-        "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
-    except:
-        message = "Unable to sign in to account try again"
-        return render(request, "landing.html", { "messg" : message, "mapbox_access_token" : config['mapboxgl']['accessToken'] })
+        try:
+            user = auth.sign_in_with_email_and_password(email, passw)
+            #uid = user['localId']
+            user = authenticate(request, username=email, password=passw)
+            if user is not None:
+                login( user)
+            else:
+                local_user = User.objects.create_user(email, email, passw)
+            # Return an 'invalid login' error message.
 
-
-    
-
-       
-
-    
+            return render(request,"dashboard.html", {
+                "firebase_apikey" : config['firebaseConfig']['apiKey'],
+                "mapbox_access_token" : config['mapboxgl']['accessToken'],
+                "firebase_authdomain" : config['firebaseConfig']['authDomain'],
+                "firebase_dburl" : config['firebaseConfig']['databaseURL'],
+                "firebase_storagebucket" : config['firebaseConfig']['storageBucket']})
+        except:
+            message = "Unable to sign in to account try again"
+            return render(request, "landing.html", { "messg" : message, "mapbox_access_token" : config['mapboxgl']['accessToken'] })
