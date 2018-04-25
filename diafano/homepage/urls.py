fff6928a336fa30e3c4a3a595b6e158419f3be26
mertaytore <aytoremert@gmail.com>
2018-04-25 19:30:13 +0300

    urls: fixes made

--- diafano/homepage/urls.py
+++ diafano/homepage/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
+from django.contrib.auth.views import logout
 from . import views
 
 urlpatterns = [
     path('', views.landing),
     path('signup/', views.signUp, name = 'signup'),
-    path('signip/', views.signIn, name = 'signin'),
+    path('signin/', views.signIn, name = 'signin'),
     path('postsignup/', views.postsignup, name = 'postsignup'),
     path('postsignin/', views.postsignin, name = 'postsignin'),
     path('postsignup_google/', views.postsignup_google, name = 'postsignup_google'),
@@ -16,6 +17,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', views.login, name='login'),
     #url(r'^logout/$', views.logout, name='logout'),
-    url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
-    url(r'^$', views.home, name='home'),
+    url(r'^auth/', include('social_django.urls', namespace='social'))  # <- Here
 ]
